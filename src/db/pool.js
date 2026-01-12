b8fe425595a4e98177eefd18e1f645cdf67bdf0a
Reñer Rabanillo <rener.g.rabanillo01@gmail.com>
2026-01-12 16:43:54 +0800

    Changed variable names

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 export default pool;
